47316cd10f117e3e2dae7c7fc37a2eaaf5cb561e
apoorva-1994 <148181148+apoorva-1994@users.noreply.github.com>
2024-09-13 15:40:46 -0700

    Add files via upload

---
+++ src/com/lc/servicenow/AscendingOrder.java
@@ -0,0 +1,16 @@
+package array;
+
+public class AscendingOrder {
+	public static void main(String[] args) {
+		int ar[] = {2,5,7,3,5,2};
+		AscendingOrder de = new AscendingOrder();
+		de.ascend(ar);
+	}
+	
+	public void ascend(int ar[]) {
+		for(int i=0;i<ar.length;i++) {
+			
+		}
+	}
+
+}
