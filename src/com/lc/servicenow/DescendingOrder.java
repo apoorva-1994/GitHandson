f806fdaeafe46d719a47a1a816f893bcb9b71125
apoorva-1994 <148181148+apoorva-1994@users.noreply.github.com>
2024-09-13 15:41:38 -0700

    Add files via upload

---
+++ src/com/lc/servicenow/DescendingOrder.java
@@ -0,0 +1,10 @@
+package array;
+
+public class DescendingOrder {
+	public static void main(String[] args) {
+		int ar[] = {2,5,7,3,5,2};
+		RemoveDuplicate de = new RemoveDuplicate();
+		de.remdup(ar);
+	}
+
+}
